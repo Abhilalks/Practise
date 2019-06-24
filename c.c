0c93911c51bc7719c5b868034816b93112cd1743
Abhilalks <46380369+Abhilalks@users.noreply.github.com>
2019-06-24 23:05:43 +0530

    Add files via upload

---
+++ c.c
@@ -0,0 +1,49 @@
+int main()
+
+{
+
+    int a[20],i,j,n,k;
+
+    printf("Enter the limit\n");
+
+    scanf("%d",&n);
+
+    printf("Enter numbers\n");
+
+    for(i=0;i<n;i++)
+
+    {
+
+        scanf("%d",&a[i]);
+
+    }
+
+    printf("Enter value for k\n");
+
+    scanf("%d",&k);
+
+    for(i=0;i<n;i++)
+
+    {
+
+        for(j=i+1;j<n;j++)
+
+        {
+
+            if((a[i]+a[j])==k)
+
+            {
+
+                printf("True for %d + %d",a[i],a[j]);
+
+            }
+
+        }
+
+    }
+
+    
+
+    return 0;
+
+}
---
+++ c1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+
+    int a[20],i,j,b[20],n;
+
+    printf("Enter a limit\n");
+
+    scanf("%d",&n);
+
+    printf("Enter the array elements\n");
+
+    for(i=0;i<n;i++)
+
+    {
+
+        scanf("%d",&a[i]);
+
+    }
+
+    for(i=0;i<n;i++)
+
+    {
+
+        b[i]=1;
+
+        for(j=0;j<n;j++)
+
+        {
+
+            if(a[i]!=a[j])
+
+            {
+
+                b[i]=a[j]*b[i];
+
+            }
+
+        }
+
+    }
+
+    printf("Expected output is\n");
+
+    for(i=0;i<n;i++)
+
+    {
+
+        printf("%d\n",b[i]);
+
+    }
+
+
+    return 0;
+
+}
